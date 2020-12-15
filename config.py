13dbea4c8768879a0940d9f548cd50c6c644c305
Praveen Saxena <saxep01@gmail.com>
2020-12-15 05:41:08 +0000

    Setup uploads to Google Cloud Storage

--- config.py
+++ config.py
@@ -3,13 +3,29 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-files = dict(
-    raw = ROOT_DIR + '/data/01_raw.csv',
-    processed = ROOT_DIR + '/data/02_processed.csv',
-    service_rules = ROOT_DIR + '/data/01_service_rules.csv'
-)
-
-signals = dict(
-    signup = ['coming', 'back', 'signup', 'signing', 'joining', 'welcome'],
-    cancellation = ['cancelled', 'cancel']
+inputs = dict(
+    data = dict(
+        filename = '01_raw.csv',
+        filedir  = ROOT_DIR + '/data',
+        fullpath = ROOT_DIR + '/data/01_raw.csv'
+    ),
+    rules = dict(
+        filename = '01_service_rules.csv',
+        filedir  = ROOT_DIR + '/data',
+        fullpath = ROOT_DIR + '/data/service_rules.csv'
+    ),
+    signals = dict(
+        signup = ['coming', 'back', 'signup', 'signing', 'joining', 'welcome'],
+        cancellation = ['cancelled', 'cancel']
+    ),
+    output = dict(
+        local = dict(
+            filedir = 'data',
+            filename = 'processed_data.csv',
+            fullpath = ROOT_DIR + '/data/processed_data.csv'
+        ),
+        cloud = dict(
+            bucket_name = 'antenna-task'
+        )
+    )
 )
