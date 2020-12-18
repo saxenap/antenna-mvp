c2a53717f2b95811cb890d63c9553e60852033b9
Praveen Saxena <saxep01@gmail.com>
2020-12-18 04:43:25 +0000

    Test printing and refactor

--- utilities.py
+++ utilities.py
@@ -1,17 +1,25 @@
 import os, sys
 import pprint
 from tabulate import tabulate 
+from IPython.display import display, HTML
+import yaml
+
 
 def save_file(file_path, content):
   os.makedirs(os.path.dirname(file_path), exist_ok=True)
   with open(file_path, "w") as f:
     f.write(content)
 
-def foutput(data):
-  return pprint.pformat(data)
+def dict_output(data):
+    print(yaml.dump(data, allow_unicode=True, default_flow_style=False))
+
+# def output(data):
+#     print(pprint.pformat(data))
+
 
 def output(data):
-    print(pprint.pformat(data))
+    display(data)
+    
 
 def doutput(data):
     print(tabulate(data, showindex=True, headers='keys', tablefmt='pretty'))
